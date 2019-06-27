030289f7132ab698a962ac3ac0da5ceca0367aae
akbar <hc.akbar@gmail.com>
2019-06-27 17:07:22 -0400

    adding h2 db connection

---
+++ src/main/java/com/akbar/app/Users.java
@@ -0,0 +1,26 @@
+package com.akbar.app;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.akbar.app")
+public class Users { //rename this
+
+    @Bean
+    public DataSource userDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+        return dataSource;
+    }
+
+
+}
